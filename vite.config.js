ebee930c41524f1ba22400a63051c85c4062f9d3
Fanny Karlsson <fanny.ll.karlsson@gmail.com>
2024-12-28 21:40:40 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    sourcemap: true, // Enable source maps for debugging
+    minify: false, // Disable minification to isolate errors
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Split large dependencies into a separate chunk
+          }
+        },
+      },
+    },
+  },
+});
